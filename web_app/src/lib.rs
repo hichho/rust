ec8dad2cf585ea9ea685eadf5b1eb3a2595f5f29
hichho <52339006+hichho@users.noreply.github.com>
2022-11-29 14:57:58 +0800

    feat:add styld_component

--- web_app/src/lib.rs
+++ web_app/src/lib.rs
@@ -1,23 +1,62 @@
 use yew::prelude::*;
+use stylist::{style,yew::styled_component, Style};
 use gloo::console::log;
 use serde::{Serialize,Deserialize};
+mod components;
+use components::atoms::main_title::MainTitle;
 #[derive(Serialize,Deserialize)]
 struct MyObject{
     username:String,
     favorite_language:String
 }
+const STYLE_FILE :&str= include_str!("main.css");
 
-#[function_component(App)]
+#[styled_component(App)]
 pub fn app()-> Html{
+    let outside_style = Style::new(STYLE_FILE).unwrap();
+    let style_sheet = style!(r#"
+    color:orange;"#).unwrap();
     let name:&str = "Brooks";
+   let stylesheet = style!(
+   r#".title{
+    color:red;
+   }"# 
+   ).unwrap(); 
    let my_object:MyObject = MyObject{
     username:name.to_owned(),
     favorite_language:"Rust".to_owned(),
    };
    let my_class = "my_title";
+   let tasks = vec!["record Video","grocery shopping","pet Xilbe"];
+   let message: Option<&str> = None;
     log!(name);
     log!(serde_json::to_string_pretty(&my_object).unwrap());
     html!{
+        <>
+       <MainTitle title="i am a component"/> 
+        <div class={outside_style}>
         <h1 class={my_class}>{"Hello World!!!"}</h1>
+        <p>{"Hi!there!"}</p>
+        if my_class != "my_class"{
+            <p>{"my class"}</p>
+        }else{
+            <p>{"i am not class"}</p>
+        }
+        if let Some(message)= message{
+            <p>{message}</p>
+        }else{
+            <p>{"no message to see"}</p>
+        }
+        <ul>{list_to_html(tasks)}</ul>
+        <h1 class={style_sheet}>{"test style_sheet"}</h1>
+        <div class={stylesheet}>
+        <div class={"title"}>{"hello style"}</div>
+        <div class={css!("color:green;font-size:75px;")}>{"hello style"}</div>
+        </div>
+        </div>
+        </>
     }
+}
+fn list_to_html(list:Vec<&str>)->Vec<Html>{
+    list.iter().map(|item|html!{<li>{item}</li>}).collect()
 }
