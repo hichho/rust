92ccfbb9b8d041e957c7ea76185cc00ca2209ed9
hichho <52339006+hichho@users.noreply.github.com>
2022-12-09 17:08:11 +0800

    feat:add "click times"

--- web_app/src/components/atoms/custom_button.rs
+++ web_app/src/components/atoms/custom_button.rs
@@ -3,10 +3,15 @@ use yew::prelude::*;
 #[derive(Properties, PartialEq)]
 pub struct Props {
     pub label: String,
+    pub onclick: Callback<()>,
 }
 #[function_component(CustomButton)]
 pub fn custom_button(props: &Props) -> Html {
+    let onclick = props.onclick.clone();
+    let button_onclick = Callback::from(move |_| {
+        onclick.emit(());
+    });
     html! {
-      <button>{&props.label}</button>
+      <button onclick={button_onclick}>{&props.label}</button>
     }
 }
