d8f0c24bc1ff8a257a53dcaaf7fed63c7d0049da
hichho <52339006+hichho@users.noreply.github.com>
2022-12-12 14:08:18 +0800

    feat:complicated state

--- web_app/src/components/molecules/custom_form.rs
+++ web_app/src/components/molecules/custom_form.rs
@@ -1,27 +1,50 @@
 use std::clone;
+use std::ops::Deref;
 
 use crate::components::atoms::custom_button::CustomButton;
 use crate::components::atoms::text_input::TextInput;
-use gloo::console::log;
 use yew::prelude::*;
+#[derive(Default, Clone)]
+struct Data {
+    pub username: String,
+    pub count: u32,
+}
 #[function_component(CustomForm)]
 pub fn custom_form() -> Html {
-    let username_state = use_state(|| "no username set".to_owned());
-    let button_count_state = use_state(|| 0_u32);
-    let cloned_username_state = username_state.clone();
-    let cloned_button_count_state = button_count_state.clone();
+    let state = use_state(|| Data::default());
+    let cloned_state = state.clone();
     let username_changed = Callback::from(move |username| {
-        cloned_username_state.set(username);
+        let mut data = cloned_state.deref().clone();
+        data.username = username;
+        cloned_state.set(data);
     });
+    let cloned_state = state.clone();
     let button_clicked = Callback::from(move |_| {
-        let count = *cloned_button_count_state;
-        cloned_button_count_state.set(count + 1);
+        let mut data = cloned_state.deref().clone();
+        data.count += 1;
+        cloned_state.set(data);
     });
+    // let username_state = use_state(|| "no username set".to_owned());
+    // let button_count_state = use_state(|| 0_u32);
+    // let cloned_username_state = username_state.clone();
+    // let cloned_button_count_state = button_count_state.clone();
+    // let username_changed = Callback::from(move |username| {
+    //     cloned_username_state.set(username);
+    // });
+    // let button_clicked = Callback::from(move |_| {
+    //     let count = *cloned_button_count_state;
+    //     cloned_button_count_state.set(count + 1);
+    // });
     html! {
+    // <div><TextInput name="username" handle_onchange={username_changed}/>
+    // <CustomButton label="Submit" onclick={button_clicked}/>
+    // <p>{"Username:"}{&*username_state}</p>
+    // <p>{"Button has been clicked"}{*button_count_state}{" times"}</p>
+    // </div>
     <div><TextInput name="username" handle_onchange={username_changed}/>
     <CustomButton label="Submit" onclick={button_clicked}/>
-    <p>{"Username:"}{&*username_state}</p>
-    <p>{"Button has been clicked"}{*button_count_state}{" times"}</p>
+    <p>{"Username:"}{&state.username}</p>
+    <p>{"Button has been clicked"}{state.count}{" times"}</p>
     </div>
       }
 }
